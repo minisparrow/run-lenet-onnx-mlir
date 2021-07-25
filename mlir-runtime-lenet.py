d2f1f9bfcddce9eda8cfc69aec4ad2ab5d77d388
minisparrow <melchior_note@163.com>
2021-07-25 08:07:49 +0000

    add time

--- mlir-runtime-lenet.py
+++ mlir-runtime-lenet.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+import time
 from PyRuntime import ExecutionSession
 
 
@@ -10,5 +11,9 @@ session = ExecutionSession(model, "run_main_graph")
 img = cv2.imread('mnist_0.png')
 
 input = np.array(img[:,:,0], np.dtype(np.float32))
+start = time.perf_counter()
 outputs = session.run(input)
+end = time.perf_counter()
+print("time execute: ")
+print(end-start)
 print(outputs)
